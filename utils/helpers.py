84dc09344b6e7b27858ac0b3621e3c55d2b6e270
itzrrj2 <providersmmin@gmail.com>
2025-04-25 17:15:56 +0530

    Add files via upload

---
+++ utils/helpers.py
@@ -0,0 +1,27 @@
+import aiohttp
+from config import CHANNEL_ID, AR_HOSTING_API
+
+async def check_user_in_channel(client, user_id):
+    try:
+        member = await client.get_chat_member(CHANNEL_ID, user_id)
+        return member.status in ("member", "administrator", "creator")
+    except:
+        return False
+
+async def get_file_size(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.head(url) as resp:
+            return int(resp.headers.get("Content-Length", 0))
+
+async def fetch_file_blob(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            return await resp.read()
+
+async def upload_to_ar(file_bytes, filename):
+    data = aiohttp.FormData()
+    data.add_field('file', file_bytes, filename=filename, content_type='application/octet-stream')
+    async with aiohttp.ClientSession() as session:
+        async with session.post(AR_HOSTING_API, data=data) as resp:
+            res = await resp.json()
+            return res.get("data")
