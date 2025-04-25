f51eb6108ed37545c106c84f7ada960217d8f745
itzrrj2 <providersmmin@gmail.com>
2025-04-25 18:15:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ BOT_TOKEN = "7079552870:AAHr1vrLw_g3Hc_EcUeECDTS3baXESC2mJo"
 ADMIN_ID = 7174055187
 CHANNEL_ID = -1002337777714
 AR_HOSTING_API = "https://ar-hosting.pages.dev/upload"
+# Required Channels (without @)
+REQUIRED_CHANNELS = ["Sr_robots", "Sr_robots_backup"]
 
 MAX_PHOTO_SIZE = 5 * 1024 * 1024
 MAX_DOCUMENT_SIZE = 50 * 1024 * 1024
